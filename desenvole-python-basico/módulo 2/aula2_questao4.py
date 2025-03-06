f628c1f9731f8071c4ea3df98a59273340429b95
EduRuizRubio <edurubio2212@gmail.com>
2025-03-06 17:17:35 -0300

    Add files via upload

---
+++ desenvole-python-basico/módulo 2/aula2_questao4.py
@@ -0,0 +1,7 @@
+juros = 1.01
+saldo = 500.0
+saldo = saldo * juros  #rendimento1
+saldo = saldo * juros  #rendimento2
+saldo = saldo * juros  #rendimento3
+print("Após 3 meses meu novo saldo é")
+print(saldo)
